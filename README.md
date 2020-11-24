6585bfb35966930dda706c5280b896a6f23bb611
martakupka <marta-k2008@ukr.net>
2020-11-24 19:17:19 +0200

    Banner added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Base layout template with Gulp, SCSS and Stylelint
-[DEMO LINK](https://martakupka.github.io/catfish_banner/)
+# CatFish Banner
+
+Stack: HTML5, BEM, SCSS, JS
+- [DEMO LINK](https://martakupka.github.io/catfish_banner/)
+
+## Description
+
+Animated CatFish banner:
+* size: 100% x 90px
+* min-width: 360px
+* max-width: 1024px
+* close button
+* redirect to another webpage on click
+
+## Local deployment
+
+### Dependencies
+* Node v12.16.3 and higher
+* NPM v6.14.4 and higher
+
+### Installing
+* Fork and clone this repository
+* Run `npm install` in your terminal
+* Run `npm start`
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const advertisement = document.querySelector('.advertisement');
+
+function closeAdvertisement() {
+  const advertisementWidth = parseFloat(
+    window.getComputedStyle(advertisement).width
+  );
+
+  const closeButton = document.querySelector('.advertisement__button');
+
+  closeButton.addEventListener('click', () => {
+    if (Math.ceil(advertisementWidth) === 1024) {
+      advertisement.style.transform = 'translate(-50%, 150%)';
+    } else {
+      advertisement.style.transform = 'translate(0, 150%)';
+    }
+  });
+}
+
+function redirectUser() {
+  advertisement.addEventListener('click', (event) => {
+    if (event.target.className !== 'advertisement__button') {
+      window.location.href = (
+        'https://www.advertserve.com/examples/adhesion.html'
+      );
+    }
+  });
+}
+
+closeAdvertisement();
+redirectUser();
